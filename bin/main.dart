45252b031d44a5e9a4d9ae5f9c44c8f9cf885f8f
liudonghua123 <liudonghua123@gmail.com>
2020-03-11 14:59:00 +0800

    initial project files using Stagehand console-full

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_native_executables/dart_native_executables.dart' as dart_native_executables;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_native_executables.calculate()}!');
+}
---
+++ test/dart_native_executables_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_native_executables/dart_native_executables.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
